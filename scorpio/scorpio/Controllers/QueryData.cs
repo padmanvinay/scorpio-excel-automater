3acfa7723887f2c4cba3e86086751a01393243ea
Vinay Padman <vinay.p@thinkpalm.com>
2023-04-26 12:18:11 +0530

    d

--- scorpio/scorpio/Controllers/QueryData.cs
+++ scorpio/scorpio/Controllers/QueryData.cs
@@ -11,6 +11,8 @@ namespace scorpio.Controllers
     public string QueryTwo { get; set; }
     public string QueryThree { get; set; }
     public string QueryFour { get; set; }
+    public string QueryFive { get; set; }
+    public string QuerySix { get; set; }
     public string vesselnumber { get; set; }
     }
 }
